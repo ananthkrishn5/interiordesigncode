9bbbd6fefb7ca9f4c5966bdc43534ad82f41a5e3
ananthkrishn5 <73382002+ananthkrishn5@users.noreply.github.com>
2024-10-23 15:17:01 +0530

    Add files via upload
    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper('.swiper-container', {
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  loop: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+});
